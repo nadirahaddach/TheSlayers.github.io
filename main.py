099a9206d2a2e8e2cf0f30d2252fb2667d6d077e
katiehickman <katie.hickman2704@gmail.com>
2022-03-11 13:56:29 -0800

    basic w password generator

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, request
 # create an instance of flask object
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("home.html")
 
-def index():
-    return render_template("layouts/index.html")
-
+@app.route('/password')
+def password():
+    return render_template('password.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
